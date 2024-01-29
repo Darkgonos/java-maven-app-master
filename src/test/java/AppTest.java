093ef613d6bc4b24f59d0b23e67237c3e4bb18c9
Oussama <o.ibnrass@gmail.com>
2024-01-29 23:34:52 +0100

    Practica Maven

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,17 +1,30 @@
-import com.example.Application;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-
 public class AppTest {
 
-    @Test
-    public void testApp() {
-        Application myApp = new Application();
-
-        String result = myApp.getStatus();
-
-        assertEquals("OK", result);
+    public int add(int a, int b) {
+        return a + b;
     }
 
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    @Test
+    public void testAddition() {
+        AppTest calculator = new AppTest();
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    public void testSubtraction() {
+        AppTest calculator = new AppTest();
+        assertEquals(2, calculator.subtract(5, 3));
+    }
+
+
+    public static void main(String[] args) {
+        org.junit.runner.JUnitCore.main("Calcul");
+    }
 }
