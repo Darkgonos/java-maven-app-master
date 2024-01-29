7d7cff10c63e89d551083745fb983b9990ef4485
Oussama <o.ibnrass@gmail.com>
2024-01-29 23:08:24 +0100

    Practica Maven

--- src/test/java/Calcul.java
+++ src/test/java/Calcul.java
@@ -1,56 +1,17 @@
+import com.example.Application;
 import org.junit.Test;
 import static org.junit.Assert.*;
+
 
 public class Calcul {
 
     @Test
-    public void testAdd() {
-        Calculator calculator = new Calculator();
-        assertEquals(5, calculator.add(2, 3));
+    public void testApp() {
+        Application myApp = new Application();
+
+        String result = myApp.getStatus();
+
+        assertEquals("OK", result);
     }
 
-    @Test
-    public void testSubtract() {
-        Calculator calculator = new Calculator();
-        assertEquals(2, calculator.subtract(5, 3));
-    }
-
-    @Test
-    public void testMultiply() {
-        Calculator calculator = new Calculator();
-        assertEquals(15, calculator.multiply(3, 5));
-    }
-
-    @Test
-    public void testDivide() {
-        Calculator calculator = new Calculator();
-        assertEquals(2, calculator.divide(6, 3));
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testDivideByZero() {
-        Calculator calculator = new Calculator();
-        calculator.divide(10, 0);
-    }
-}
-
-class Calculator {
-    public int add(int a, int b) {
-        return a + b;
-    }
-
-    public int subtract(int a, int b) {
-        return a - b;
-    }
-
-    public int multiply(int a, int b) {
-        return a * b;
-    }
-
-    public int divide(int a, int b) {
-        if (b == 0) {
-            throw new IllegalArgumentException("No se puede dividir por cero");
-        }
-        return a / b;
-    }
 }
