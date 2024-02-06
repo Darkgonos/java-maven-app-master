56e31ad37a514f79cfe682853dbe4daf3b3f8f85
Oussama <o.ibnrass@gmail.com>
2024-02-06 23:37:16 +0100

    Docker Hub y Script Groovy

--- script.groovy
+++ script.groovy
@@ -4,11 +4,6 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'Docker-hub', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-        sh 'docker build -t darkgonos/practica:jma-1.1.0 .'
-        sh "docker login -u $USERNAME -p $PASSWORD"
-        sh 'docker push darkgonos/practica:jma-1.1.0'
-    }
 } 
 
 def deployApp() {
